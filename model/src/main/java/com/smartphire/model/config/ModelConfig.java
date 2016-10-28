1d0d3ec9215006bf74ba7dc85773669e81a99220
Alex Voinovan <avoinovan@yapstone.com>
2016-10-28 11:17:11 -0700

    Fixes issue #1

---
+++ model/src/main/java/com/smartphire/model/config/ModelConfig.java
@@ -0,0 +1,6 @@
+package com.smartphire.model.config;
+
+@org.springframework.context.annotation.Configuration
+public class ModelConfig {
+
+}
---
+++ webapp/src/main/java/com/smartphire/model/webapp/ConturApplication.java
@@ -0,0 +1,12 @@
+package com.smartphire.model.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConturApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConturApplication.class, args);
+	}
+}
